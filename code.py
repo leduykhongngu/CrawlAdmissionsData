df038ac728c2e2a401298c403b434a9008fd22ef
Le Duy Thuc <leduythuc.cs@gmail.com>
2021-06-16 20:34:06 +0700

    Use black to format

--- code.py
+++ code.py
@@ -14,22 +14,22 @@ def FormatString(s: str):
 def FormatInt(s: str):
     s = FormatString(s)
     s = re.search(r"\d*", s)
-    if (s == None):
+    if s is None:
         return 0
     s = s[0]
-    if (len(s) == 0):
+    if len(s) == 0:
         return 0
     return int(s)
 
 
 def FormatFloat(s: str):
-    s = FormatString(s.replace(',', '.'))
+    s = FormatString(s.replace(",", "."))
     s = re.search(r"\d*\.*\d*", s)
-    if (s == None):
-        return ''
+    if s is None:
+        return ""
     s = s[0]
-    if (len(s) == 0):
-        return ''
+    if len(s) == 0:
+        return ""
     return float(s)
 
 
@@ -46,24 +46,27 @@ class Studen:
         this.nvC1 = FormatString(data[8].td.string)
         this.nvC2 = FormatString(data[9].td.string)
         this.PriorityPoints = FormatFloat(data[10].td.string)
-        data = data[11].find_all('th')
+        data = data[11].find_all("th")
         this.SVan = FormatFloat(data[7].string)
         this.SAnh = FormatFloat(data[8].string)
         this.SToan = FormatFloat(data[9].string)
         this.SC1 = FormatFloat(data[10].string)
         this.SC2 = FormatFloat(data[11].string)
-        tmp = re.findall(r": [\w0-9\.]*\r", data[12].text.replace(',', '.'))
+        tmp = re.findall(r": [\w0-9\.]*\r", data[12].text.replace(",", "."))
         this.SumTVA = FormatFloat(tmp[0])
         this.SumC1 = FormatFloat(tmp[1])
         this.SumC2 = FormatFloat(tmp[2])
 
 
 Prefix = "http://sgdbinhduong.edu.vn/Tracuudiem/Tuyensinh10/tabid/294/NamThi/2020/DotThiId/34/ThiSinhId/"
+
 Suffix = "/Default.aspx"
-Line = [u'Tên', u'SBD', u'Ngày sinh', u'Giới Tính', u'Nơi sinh', u'Trường', u'Nguyện vọng 1', u'Nguyện vọng 2', u'Chuyên 1',
-        u'Chuyên 2', u'Điểm ưu tiên', u'Văn', u'Anh', u'Toán', u'Chuyên 1', u'Chuyên 2', u'Tổng', u'Tổng chuyên 1', u'Tổng chuyên 2']
-with open('Tuyensinh10.csv', 'ab') as writeFile:
-    writer = csv.writer(writeFile, encoding='utf-8')
+
+Line = [u"Tên", u"SBD", u"Ngày sinh", u"Giới Tính", u"Nơi sinh", u"Trường", u"Nguyện vọng 1",
+        u"Nguyện vọng 2", u"Chuyên 1", u"Chuyên 2", u"Điểm ưu tiên", u"Văn", u"Anh", u"Toán",
+        u"Chuyên 1", u"Chuyên 2", u"Tổng", u"Tổng chuyên 1", u"Tổng chuyên 2"]
+with open("Tuyensinh10.csv", "ab") as writeFile:
+    writer = csv.writer(writeFile, encoding="utf-8")
     writer.writerow(Line)
     L = 149774
     # R = 149780
@@ -71,9 +74,10 @@ with open('Tuyensinh10.csv', 'ab') as writeFile:
     for i in range(L, R+1):
         print("Geting", i)
         x = requests.get(Prefix+str(i)+Suffix).text
-        soup = BeautifulSoup(x, 'html.parser')
-        Test = Studen(soup.tbody.find_all('tr'))
-        writer.writerow([Test.name, Test.SBD, Test.birthday, Test.sex, Test.BornIn, Test.SecSchool, Test.nv1, Test.nv2, Test.nvC1,
-                         Test.nvC2, Test.PriorityPoints, Test.SVan, Test.SAnh, Test.SToan, Test.SC1, Test.SC2, Test.SumTVA, Test.SumC1, Test.SumC2])
-        #print("Tên {}\nSBD {}\nNgày sinh {}\nGiới Tính {}\nNơi sinh {}\nTrường {}\nNguyện vọng 1 {}\nNguyện vọng 2 {}\nChuyên 1 {}\nChuyên 2 {}\nĐiểm ưu tiên {}\nVăn {}\nAnh {}\nToán {}\nChuyên 1 {}\nChuyên 2 {}\nTổng {}\nTổng chuyên 1 {}\nTổng chuyên 2 {}\n".format(Test.name,Test.SBD,Test.birthday,Test.sex,Test.BornIn,Test.SecSchool,Test.nv1,Test.nv2,Test.nvC1,Test.nvC2,Test.PriorityPoints,Test.SVan,Test.SAnh,Test.SToan,Test.SC1,Test.SC2,Test.SumTVA,Test.SumC1,Test.SumC2))
+        soup = BeautifulSoup(x, "html.parser")
+        Test = Studen(soup.tbody.find_all("tr"))
+        writer.writerow([Test.name, Test.SBD, Test.birthday, Test.sex, Test.BornIn, Test.SecSchool,
+                         Test.nv1, Test.nv2, Test.nvC1, Test.nvC2, Test.PriorityPoints, Test.SVan,
+                         Test.SAnh, Test.SToan, Test.SC1, Test.SC2, Test.SumTVA, Test.SumC1, Test.SumC2])
+
 writeFile.close()
